e3c42cf5ab2f4a15b33b89035327ead584e4db80
Tyler C <tyler@candeegenerations.com>
2024-03-17 10:09:49 -0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import dayjs from 'dayjs'
 import duration from 'dayjs/plugin/duration'
 import relativeTime from 'dayjs/plugin/relativeTime'
+import {Head} from 'next/document'
 import {useEffect, useState} from 'react'
 
 dayjs.extend(duration)
