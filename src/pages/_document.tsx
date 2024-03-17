abfee3cd76a98987be54220cef229c48998e66c6
Tyler C <tyler@candeegenerations.com>
2024-03-17 09:55:03 -0400

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,10 +3,6 @@ import {Head, Html, Main, NextScript} from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <title>Countdown</title>
-      </Head>
-
       <body className="text-base text-white" style={{backgroundColor: '#621A34'}}>
         <main className="p-10 pb-4">
           <Main />
