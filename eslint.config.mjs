ef0c22e3e029790ce68226a74d2b24f1ea5890e6
Tyler Candee <tyler@candeegenerations.com>
2024-05-07 08:57:41 -0400

    chore: build fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,6 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    ignores: ['src/graphql/index.tsx'],
     rules: {
       '@typescript-eslint/ban-ts-comment': 0,
       '@typescript-eslint/no-empty-function': 0,
